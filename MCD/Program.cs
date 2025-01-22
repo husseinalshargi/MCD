d24b62e2793a97c022af4fe33e939282156de421
Hussein Adel <x.sky.x@hotmail.com>
2025-01-22 18:12:34 +0300

    add db context for the db

--- MCD/Program.cs
+++ MCD/Program.cs
@@ -1,7 +1,16 @@
+using MCD.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // to Add services to the container. -> add in thus file
 builder.Services.AddControllersWithViews();
+// to add the app db context
+builder.Services.AddDbContext<ApplicationDbContext>(options=>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))); // x=>y : lambda where x is the input and y is the function
+
+
+
 
 var app = builder.Build();
 //when you add new app settings edit here
@@ -23,8 +32,5 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-//ff
-//ff
-// third comment
-// test test
+
 app.Run();
