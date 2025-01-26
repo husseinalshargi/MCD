70277ab9550518d8330ddd75317f69c9f1623ad8
Hussein Adel <x.sky.x@hotmail.com>
2025-01-26 19:43:10 +0300

    add document table with the migration

--- MCD.DataAccess/Data/ApplicationDbContext.cs
+++ MCD.DataAccess/Data/ApplicationDbContext.cs
@@ -6,8 +6,10 @@ namespace MCD.DataAccess.Data
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)//just configeration
-        {
-
-        }
+        { }
+        // after creating the table content in the model create a dbset object here in order to add it in the db
+        // after that add a new migration to create a table with the name of (the variable name
+        // then update the database (Package Manager Console)
+        public DbSet<Document> Documents { get; set; }
     }
 }
---
+++ MCD.Models/Document.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD.Models
+{
+    public class Document //after creating the table in the model we create an object in the data access -> application db context
+    {
+        //[--] -> validation
+        //Document table content:
+        //the document id is the key
+        [Key]
+        public int Id { get; set; }
+        
+        //to add a foregin key (uncomment after adding all tables...)
+        public int UserId { get; set; }
+        //[ForeignKey("UserId")]
+        //public User user { get; set; }
+
+        public int CategoryId { get; set; }
+        //[ForeignKey("CategoryId")]
+        //public Category category { get; set; }
+
+        [Required] //to be requierd in the data validation when entering the data
+        [Range(1,50)]
+        public required string Title { get; set; }
+
+        [Required] //to be requierd in the data validation when entering the data
+        [Range(1, 50)]
+        public required string FileName { get; set; } // to use in path (which will be in wwwroot)\ FileName.FileType
+
+        [Required]
+        public required string FileType { get; set; } // PDF, JPG, txt, etc...
+
+        [Required]
+        [DataType(DataType.Date)] //to ensure that the type will be date
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd HH:mm:ss}")] //we need the zero to make it a placeholder to insert the datetime in the template
+        public required DateTime UploadDate { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd HH:mm:ss}")]
+        public required DateTime UpdateDate { get; set; } 
+
+        [Required] 
+        public required string AITaskStatus { get; set; } // processing processed etc..
+    }
+}
