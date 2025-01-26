dfa367c063b808eecc13232171b5fdf97d127b41
Hussein Adel <x.sky.x@hotmail.com>
2025-01-26 17:58:51 +0300

    project organized into separate layers or components (services)

--- MCD.DataAccess/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MCD.DataAccess
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ MCD.Utility/SD.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD.Utility
+{
+    public static class SD // static details for all the constants of our project
+    {
+    }
+}
