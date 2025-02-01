2e3319c72dd85ee1051256631d9771989dde1c1a
Hussein Adel <x.sky.x@hotmail.com>
2025-02-01 22:23:01 +0300

    creating Repository an IRepository for dealing with CRUD operations before we make adjustments for each class (table) as needed

---
+++ MCD.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD.DataAccess.Repository.IRepository
+{
+    public interface IRepository<T> where T : class //here are the methods in the basic crud operations (without implementation)
+        //for more details we could create an interface also a class of the class (table) that needs further adjustments 
+    {
+        //CRUD:
+
+        //create: 
+        void Add(T entity);
+
+
+        //retrieve:  
+
+        //when it is implemented it will be in T class (could be any class)
+        // T -> any class (any table in the db) to preform CRUD operations
+        //return all objects
+        IEnumerable<T> GetAll(); // return type is a collection of objects of the type T -> to get all objects (instances of a T table)
+        // IEnumerable is the best way of iterating over a collection without modifying it Note: no indexing, use id
+        // get only one item -> in case you want to edit or delete it so no i enumerable
+        T Get(Expression<Func<T, bool>> filter); // Func<T, bool> is a function that takes an object of type T and returns true or false
+        // in order to use lambda in it 
+        // expression is a wrapper that translate the function into SQL queries (in EF framework) best way to deal with db
+
+
+        //update: if we wanted to do so but it is not included because each class have its way of updating also not all fields will be updated
+        // so it will be defined when it is required 
+        //void Update(T entity);
+
+        //delete:
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entity); //to delete a range of values also IEnumerable is used here as it is the best collection for db items
+
+
+    }
+}
---
+++ MCD.DataAccess/Repository/Repository.cs
@@ -0,0 +1,60 @@
+﻿using MCD.DataAccess.Data;
+using MCD.DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T: class //Repository<T> → A generic class that works with any data type T, then where... to ensure that T always is a reference type 
+    {
+        //always do not forget to put _db.SaveChanges() after doing something with the db values
+        // in order to do something with the db we must do dependency injection:allows objects to receive their dependencies from an external source instead of creating them internally
+        // rather than always creating db object for dealing with db (more than one time which will put more coupling) do it one time and pass the reference
+
+        private readonly ApplicationDbContext _db; //create an object of the class responsible of dealing with db
+        internal DbSet<T> dbSet; // as we can't use T when dealing with objects in the table (it doesn't know that we want that) so we will use an alternative to that which is creating a set of T
+        public Repository(ApplicationDbContext db) //assign the db object
+        {
+            _db = db;
+            this.dbSet = _db.Set<T>(); // after creating the dbSet here we will put the type that we need to preform the operation on
+            // example:
+            // _db.Table == dbSet as we refiring to the table with T
+        }
+
+
+        public void Add(T entity)
+        {
+            //after creating the dbSet object 
+            dbSet.Add(entity); // to add an instance
+        }
+
+        public T Get(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = dbSet; //return all the queries and make it a quarriable object as we are getting one of it
+            query = query.Where(filter); // all the objects that are filtered will be here
+            return query.FirstOrDefault(); // first instance in the filter
+
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = dbSet;
+            return query.ToList(); //return all without the filter
+        }
+
+        public void Remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entity)
+        {
+            dbSet.RemoveRange(entity); // RemoveRange expects an IEnumerable object
+        }
+    }
+}
