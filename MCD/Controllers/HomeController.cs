b41caec2a0409afe8c67dd87521397b00bb8fd57
VuineC <21423232ksa@aou.edu.sa>
2025-02-05 10:30:11 +0300

    commit

--- MCD/Controllers/HomeController.cs
+++ MCD/Controllers/HomeController.cs
@@ -23,6 +23,10 @@ namespace MCD.Controllers
             return View();
         }
 
+        public IActionResult Document()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
