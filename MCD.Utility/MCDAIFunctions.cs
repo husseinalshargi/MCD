77824b9820ef36955840074b59c4653a1f4dbe29
Hussein Adel <x.sky.x@hotmail.com>
2025-03-25 06:10:41 +0300

    ai

---
+++ MCD.Utility/MCDAIFunctions.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD.Utility
+{
+    public class MCDAIFunctions
+    {
+        private static readonly HttpClient _httpClient = new HttpClient
+        {
+            BaseAddress = new Uri("http://0.0.0.0:8001/") // Ensure this matches your API
+        };
+        public static async Task<string> PostAsync<T>(string endpoint, T data)
+        {
+            string jsonContent = JsonConvert.SerializeObject(data);
+            HttpContent content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = await _httpClient.PostAsync(endpoint, content);
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadAsStringAsync();
+        }
+
+    }
+}
---
+++ MCD/Areas/Customer/Controllers/test.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MCD.Utility;
+using System;
+
+namespace MCD.Areas.Customer.Controllers
+{
+    public class Test : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            string filePath = @"C:\\Users\\xskyx\\OneDrive\\سطح المكتب\\Gr118 book\\GR118 Ch5.pdf"; // Change to your actual file path
+
+            using (FileStream stream = File.OpenRead(filePath))
+            { 
+                var request = DriveService.Files.Create(FileMetaData, stream, model.DocumentFile.ContentType);
+                request.Fields = "id, webViewLink"; //  file id and link
+                var uploadedFile = await request.UploadAsync();
+
+                if (uploadedFile.Status != Google.Apis.Upload.UploadStatus.Completed) //if there is an error with the file uploading
+                {
+                    return StatusCode(500, "Error uploading file to Google Drive.");
+                }
+                // Get uploaded file details
+                var fileData = request.ResponseBody;
+                //in order to grant the user access to the file after creating it (making the file not accessible by someone isn't the owner)
+                GoogleDriveService.GiveFilePermission(DriveService, "writer", fileData.Id, userEmail);
+
+            }
+            string response = await TestMCDClient.PostAsync("api/users", newUser);
+            ViewBag.ApiResponse = response;
+
+            return View();
+
+        }
+    }
+}
